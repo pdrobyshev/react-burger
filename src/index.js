782ed9e9bee66a04722ee333f7d888937f599d56
pdrobyshev <pdrobyshev@wooppay.com>
2021-06-21 12:46:43 +0600

    init store in separate file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-
-import { rootReducer } from './services';
 
 import './index.scss';
 import App from './components/app/app';
-
-const composeEnhancers =
-	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-		: compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
+import { store } from './services/store';
 
 ReactDOM.render(
 	<React.StrictMode>
