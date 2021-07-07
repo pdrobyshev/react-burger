9ed0761d6d5393220e789845f75efd4bd1d14b56
pdrobyshev <pa.drobyshev@gmail.com>
2021-07-07 23:44:52 +0600

    added exact to routes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,37 +10,47 @@ const App = () => (
       <AppHeader />
 
       <Switch>
-        <Route path="/" exact>
+        <Route path='/' exact>
           <Constructor />
         </Route>
-        <Route path="/login" exact>
+        //TODO сделать защищенный роут для авторизованного пользователя
+        {/* если залогинен - при переходе на страницу редиректить на главную */}
+        <Route path='/login' exact>
           <Login />
         </Route>
-        <Route path="/register" exact>
+        //TODO сделать защищенный роут для авторизованного пользователя
+        {/* если залогинен - при переходе на страницу редиректить на главную */}
+        <Route path='/register' exact>
           <Register />
         </Route>
-        <Route path="/forgot-password" exact>
+        <Route path='/forgot-password' exact>
           <ForgotPassword />
         </Route>
-        <Route path="/reset-password" exact>
+        //TODO создать protected route component with reset
+        {/* нужно проверять что пришли с урла forgot-password, иначе - редирект на главную */}
+        <Route path='/reset-password' exact>
           <ResetPassword />
         </Route>
-        <Route path="/feed" exact>
+        <Route path='/feed' exact>
           <Feed />
         </Route>
-        <Route path="/feed/:id" exact>
+        <Route path='/feed/:id' exact>
           {/*<FeedOrder />*/}
         </Route>
-        <Route path="/profile" exact>
+        //TODO создать protected route component
+        {/*если залогинен - вернуть children*/}
+        {/*иначе - редирект на страницу логина, а в state передать location "откуда" пришли*/}
+        {/*чтобы после авторизации вернуться на защищенный роут куда хотели попасть*/}
+        <Route path='/profile' exact>
           {/*<Profile />*/}
         </Route>
-        <Route path="/profile/orders" exact>
+        <Route path='/profile/orders' exact>
           {/*<History />*/}
         </Route>
-        <Route path="/profile/orders/:id" exact>
+        <Route path='/profile/orders/:id' exact>
           {/*<HistoryOrder />*/}
         </Route>
-        <Route path="/ingredients/:id" exact>
+        <Route path='/ingredients/:id' exact>
           {/*<IngredientPage />*/}
         </Route>
         <Route>
