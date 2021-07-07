a82781224293d022cbd01fba19611f8b7c92a195
pdrobyshev <pa.drobyshev@gmail.com>
2021-07-07 23:24:00 +0600

    show login link in header if not authorized

--- src/components/app-header/profile-link/profile-link.js
+++ src/components/app-header/profile-link/profile-link.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from './profile-link.module.scss';
 import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import LinkItem from '../../link-item/link-item';
 
 const Profile = () => {
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   return (
     <div className={styles.profile}>
-      <LinkItem Icon={ProfileIcon} text="Личный кабинет" to="/profile" />
+      {isLoggedIn ? (
+        <LinkItem Icon={ProfileIcon} text="Личный кабинет" to="/profile" />
+      ) : (
+        <LinkItem Icon={ProfileIcon} text="Войти" to="/login" />
+      )}
     </div>
   );
 };
