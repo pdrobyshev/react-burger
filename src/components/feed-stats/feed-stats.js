e13eafa4826074660469b029f857361ee1ed1ea3
pdrobyshev <pdrobyshev@wooppay.com>
2021-07-23 14:35:59 +0600

    feed stats styles

--- src/components/feed-stats/feed-stats.js
+++ src/components/feed-stats/feed-stats.js
@@ -5,30 +5,30 @@ import styles from './feed-stats.module.scss';
 export const FeedStats = () => (
   <section className={styles.feedStats}>
     <div className={styles.flexWrapper}>
-      <div className={styles.ordersList}>
-        <span className={`${styles.textTitle}`}>Готовы:</span>
-        <span className={`${styles.smallDigits}  ${styles.textGreen}`}>034533</span>
-        <span className={`${styles.smallDigits}  ${styles.textGreen}`}>034533</span>
-        <span className={`${styles.smallDigits}  ${styles.textGreen}`}>034533</span>
-        <span className={`${styles.smallDigits}  ${styles.textGreen}`}>034533</span>
-        <span className={`${styles.smallDigits}  ${styles.textGreen}`}>034533</span>
+      <div className={styles.orderNumbers}>
+        <span className={styles.orderStatus}>Готовы:</span>
+        <span className={`${styles.digits} ${styles.textGreen}`}>034533</span>
+        <span className={`${styles.digits} ${styles.textGreen}`}>034533</span>
+        <span className={`${styles.digits} ${styles.textGreen}`}>034533</span>
+        <span className={`${styles.digits} ${styles.textGreen}`}>034533</span>
+        <span className={`${styles.digits} ${styles.textGreen}`}>034533</span>
       </div>
-      <div className={styles.ordersList}>
-        <span className={`${styles.textTitle}`}>В работе:</span>
-        <span className={`${styles.smallDigits}`}>034533</span>
-        <span className={`${styles.smallDigits}`}>034533</span>
-        <span className={`${styles.smallDigits}`}>034533</span>
+      <div className={styles.orderNumbers}>
+        <span className={styles.orderStatus}>В работе:</span>
+        <span className={styles.digits}>034533</span>
+        <span className={styles.digits}>034533</span>
+        <span className={styles.digits}>034533</span>
       </div>
     </div>
 
     <div className={styles.totalWrapper}>
-      <span className={`${styles.totalTitle}`}>Выполнено за все время:</span>
-      <span className={`${styles.digits}`}>28 752</span>
+      <span className={styles.totalTitle}>Выполнено за все время:</span>
+      <span className={`${styles.digits} ${styles.digitsLarge}`}>28 752</span>
     </div>
 
     <div className={styles.totalWrapper}>
-      <span className={`${styles.totalTitle}`}>Выполнено за сегодня:</span>
-      <span className={`${styles.digits}`}>138</span>
+      <span className={styles.totalTitle}>Выполнено за сегодня:</span>
+      <span className={`${styles.digits} ${styles.digitsLarge}`}>138</span>
     </div>
   </section>
 );
