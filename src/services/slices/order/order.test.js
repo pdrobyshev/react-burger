3f3035fbd65a0cdf341b2126356b5e5c0694b3bf
pdrobyshev <pdrobyshev@wooppay.com>
2021-07-28 00:59:42 +0600

    fix order tests

--- src/services/slices/order/order.test.js
+++ src/services/slices/order/order.test.js
@@ -1,4 +1,11 @@
-import orderReducer, { initialState, setOrderElementsIds, resetOrder, closeOrderModal } from './order';
+import orderReducer, {
+  initialState,
+  setOrderElementsIds,
+  resetOrder,
+  closeOrderModal,
+  createOrder,
+  getOrderInfo,
+} from './order';
 
 describe('order reducer', () => {
   it('should return the initial state', () => {
@@ -14,14 +21,73 @@ describe('order reducer', () => {
   });
 
   it('should reset order', () => {
-    const reducer = orderReducer({ ...initialState, order: {} }, resetOrder());
+    const reducer = orderReducer(initialState, resetOrder());
     const result = { ...initialState, order: null };
     expect(reducer).toEqual(result);
   });
 
   it('should handle modal close', () => {
-    const reducer = orderReducer({ ...initialState, isOrderModalOpened: true }, closeOrderModal());
+    const reducer = orderReducer(initialState, closeOrderModal());
     const result = { ...initialState, isOrderModalOpened: false };
+    expect(reducer).toEqual(result);
+  });
+
+  it('should handle createOrder pending', () => {
+    const action = { type: createOrder.pending.type };
+    const reducer = orderReducer(initialState, action);
+    const result = { ...initialState, isLoading: true };
+    expect(reducer).toEqual(result);
+  });
+
+  it('should handle createOrder fulfilled', () => {
+    const action = {
+      type: createOrder.fulfilled.type,
+      payload: {
+        order: {
+          number: '1241',
+        },
+      },
+    };
+    const reducer = orderReducer(initialState, action);
+    const result = {
+      ...initialState,
+      isLoading: false,
+      orderId: action.payload.order.number,
+      isOrderModalOpened: true,
+    };
+    expect(reducer).toEqual(result);
+  });
+
+  it('should handle createOrder rejected', () => {
+    const action = { type: createOrder.rejected.type };
+    const reducer = orderReducer(initialState, action);
+    const result = { ...initialState, orderId: null, isLoading: false };
+    expect(reducer).toEqual(result);
+  });
+
+  it('should handle getOrderInfo pending', () => {
+    const action = { type: getOrderInfo.pending.type };
+    const reducer = orderReducer(initialState, action);
+    const result = { ...initialState, isLoading: true };
+    expect(reducer).toEqual(result);
+  });
+
+  it('should handle getOrderInfo fulfilled', () => {
+    const action = {
+      type: getOrderInfo.fulfilled.type,
+      payload: {
+        orders: [{}],
+      },
+    };
+    const reducer = orderReducer(initialState, action);
+    const result = { ...initialState, isLoading: false, order: action.payload.orders[0] };
+    expect(reducer).toEqual(result);
+  });
+
+  it('should handle getOrderInfo rejected', () => {
+    const action = { type: getOrderInfo.rejected.type };
+    const reducer = orderReducer(initialState, action);
+    const result = { ...initialState, order: null, isLoading: false };
     expect(reducer).toEqual(result);
   });
 });
